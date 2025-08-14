f2215e82b5499a0222ff0de20cf9bdddf72fa9d5
Real-Luxof <luxtheluxof@gmail.com>
2025-08-14 15:02:32 +0500

    stupid gold sword could block wtf

--- src/main/java/com/luxof/lapisworks/items/GoldSword.java
+++ src/main/java/com/luxof/lapisworks/items/GoldSword.java
@@ -3,11 +3,15 @@ package com.luxof.lapisworks.items;
 import com.luxof.lapisworks.items.shit.AmelSword;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.ToolMaterials;
+import net.minecraft.util.UseAction;
 
 public class GoldSword extends AmelSword {
     public GoldSword() {
         super(ToolMaterials.GOLD, 3, -2.4F, new Item.Settings(), Items.GOLD_INGOT);
     }
+    @Override
+    public UseAction getUseAction(ItemStack stack) { return UseAction.NONE; }
 }
