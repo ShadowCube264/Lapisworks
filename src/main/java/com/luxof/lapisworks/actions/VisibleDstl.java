348845138ad2e22e914eacbff8fcef12baebb56f
Real-Luxof <luxtheluxof@gmail.com>
2025-08-18 16:09:57 +0500

    1.3.0 tidied up and added docs, new patterns should work but i don't have much faith in spheric distl

--- src/main/java/com/luxof/lapisworks/actions/VisibleDstl.java
+++ src/main/java/com/luxof/lapisworks/actions/VisibleDstl.java
@@ -8,12 +8,17 @@ import at.petrak.hexcasting.api.casting.eval.vm.CastingImage;
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation;
 import at.petrak.hexcasting.api.casting.iota.BooleanIota;
 import at.petrak.hexcasting.api.casting.iota.Iota;
+import at.petrak.hexcasting.api.casting.mishaps.MishapBadCaster;
 import at.petrak.hexcasting.api.misc.MediaConstants;
 
 import static com.luxof.lapisworks.Lapisworks.clamp;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.luxof.lapisworks.MishapThrowerJava;
+
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
@@ -24,8 +29,12 @@ import net.minecraft.world.RaycastContext.ShapeType;
 public class VisibleDstl implements ConstMediaAction {
     @Override
     public List<Iota> execute(List<? extends Iota> args, CastingEnvironment ctx) {
+        Optional<LivingEntity> casterOp = Optional.of(ctx.getCastingEntity());
+        if (casterOp.isEmpty()) { MishapThrowerJava.throwMishap(new MishapBadCaster()); }
         Vec3d start = OperatorUtils.getVec3(args, 1, getArgc());
         Vec3d endPoint = OperatorUtils.getVec3(args, 0, getArgc());
+        ctx.assertVecInRange(start);
+        ctx.assertVecInRange(endPoint);
         Vec3d temp = endPoint.subtract(start);
         // real end point must be 1 block before the chosen end point.
         // yeah, this'll mean pure diagonals are funky, but whatever.
@@ -35,7 +44,7 @@ public class VisibleDstl implements ConstMediaAction {
             clamp(temp.z, -1.0, 1.0)
         ));
         BlockHitResult hitResult = ctx.getWorld().raycast(
-            new RaycastContext(start, realEnd, ShapeType.OUTLINE, FluidHandling.NONE, null)
+            new RaycastContext(start, realEnd, ShapeType.OUTLINE, FluidHandling.NONE, casterOp.get())
         );
         return List.of(new BooleanIota(hitResult.getType() == HitResult.Type.MISS));
     }
