2a990753177a5e31b6123b886306f717661b1769
Real-Luxof <luxtheluxof@gmail.com>
2025-09-04 22:20:28 +0500

    work exclamation mark!

--- doc/src/hexdoc_lapisworks/book/pages/pages.py
+++ doc/src/hexdoc_lapisworks/book/pages/pages.py
@@ -2,7 +2,8 @@ from typing import Self
 
 from hexdoc_hexcasting.book.page.abstract_pages import PageWithOpPattern # pyright: ignore[reportMissingTypeStubs]
 from hexdoc_hexcasting.metadata import HexContext
-from hexdoc_hexcasting.utils.pattern import PatternInfo
+from hexdoc_hexcasting.utils.pattern import PatternInfo, Direction
+from hexdoc.core.resource import ResourceLocation
 from pydantic import ValidationInfo, model_validator
 
 # Look mom, I'm here. Very top of Arasaka tower.
@@ -15,16 +16,12 @@ class LookupPWShapePage(PageWithOpPattern, type="hexcasting:lapisworks/pwshape")
     
     @model_validator(mode="after")
     def _post_root_lookup(self, info: ValidationInfo):
-        hex_ctx = HexContext.of(info)
-        self._patterns = [hex_ctx.patterns[self.op_id]]
+        #hex_ctx = HexContext.of(info)
+        self._patterns = [PatternInfo(id=ResourceLocation("lapisworks", "tmp"), startdir=Direction.NORTH_WEST, signature="aqaeawdwwwdwqwdwwwdweqaaaqaqwdddedeewqded", is_per_world=True)]
         return self
     
     @model_validator(mode="after")
     def _check_anchor(self) -> Self:
         if str(self.op_id) != self.anchor:
             raise ValueError(f"op_id={self.op_id} does not equal anchor={self.anchor}")
-        # at least TRY to get it to work, this is temp to select the first
-        # perworldshape pat for this next commit push
-        self.op_id += "0"
-        self.anchor += "0" # type: ignore
         return self
