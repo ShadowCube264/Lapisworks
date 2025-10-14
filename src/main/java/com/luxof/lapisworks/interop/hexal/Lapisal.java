14fcd5878d316372f667c6b8438e59d358db83c9
Real-Luxof <luxtheluxof@gmail.com>
2025-10-14 14:26:30 +0500

    test with hexdoc's latest version, should fix crafting recipe bug

---
+++ src/main/java/com/luxof/lapisworks/interop/hexal/Lapisal.java
@@ -0,0 +1,9 @@
+package com.luxof.lapisworks.interop.hexal;
+
+// Lapisal, then like, maybe Hexaworks or Hexalapis for Hexal's Lapisworks interop?
+// coming up with names regardless of if they'll ever be used is fun
+public class Lapisal {
+    public static void beCool() {
+        
+    }
+}
